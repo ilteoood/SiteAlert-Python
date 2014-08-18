a1fbdc4931d99127692f2d3a18bc68d439f4909a
ilteoood <teo_il_tecktoniko@hotmail.it>
2014-08-18 14:46:15 +0200

    Layout improvements

--- SiteAlert.py
+++ SiteAlert.py
@@ -34,10 +34,13 @@ import sys
 import platform
 from os.path import expanduser
 
+
 def findHome():
     return expanduser("~")
 
-path=findHome()+os.sep+"SiteAlert"+os.sep
+
+path = findHome() + os.sep + "SiteAlert" + os.sep
+
 
 def clearScreen():
     if platform.system() == "Windows":
@@ -45,9 +48,11 @@ def clearScreen():
     else:
         os.system("clear")
 
+
 def visualizeMenu():
     clearScreen()
     print("What do you want to do?\n1) Display sites\n2) Add new site to check\n3) Fetch a site from the config file\n4) Check sites\n5) Delete a site\n6) Exit")
+
 
 def choice():
     clearScreen()
@@ -61,25 +66,29 @@ def choice():
     except ValueError:
         return 6
 
+
 def findDirs():
     if not os.path.exists(path):
         os.mkdir(path)
     return os.listdir(path)
 
+
 def displaySites():
     try:
-        i=1
-        print ("0) Exit")
-        for dir in os.listdir(findHome()+os.sep+"SiteAlert"+os.sep):
-            print (str(i)+") "+dir)
-            i=i+1
+        i = 1
+        print("0) Exit")
+        for dir in os.listdir(findHome() + os.sep + "SiteAlert" + os.sep):
+            print(str(i) + ") " + dir)
+            i = i + 1
     except OSError:
         return
 
+
 def stdURL(site):
     if not site.startswith("http://"):
-        site="http://"+site
+        site = "http://" + site
     return site
+
 
 def saveFile(path, site, mail, urli):
     fw = open(path, "w")
@@ -87,159 +96,166 @@ def saveFile(path, site, mail, urli):
     fw.write(mail)
     fw.write(str(urli.read()))
     fw.close()
-    print ("Site saved correctly!")
+    print("Site saved correctly!")
+
 
 def addSite(site, nameSite, mail):
-    if site=="" or nameSite=="" or mail=="":
-        print ("Insert the link for the site: ")
-        site=input()+"\n"
-        print ("Insert a name for the site: ")
-        nameSite=input()
-        print ("Insert the email where you want to be informed: (if you want to add other mail, separate them with \";\")")
-        mail=input()+"\n"
+    if site == "" or nameSite == "" or mail == "":
+        print("Insert the link for the site: ")
+        site = input() + "\n"
+        print("Insert a name for the site: ")
+        nameSite = input()
+        print("Insert the email where you want to be informed: (if you want to add other mail, separate them with \";\")")
+        mail = input() + "\n"
     try:
-        urli=urllib.request.urlopen(stdURL(site))
-        responseCode=urli.getcode()
+        urli = urllib.request.urlopen(stdURL(site))
+        responseCode = urli.getcode()
         if responseCode == 200:
-            pathMod=path+nameSite+os.sep
+            pathMod = path + nameSite + os.sep
             if not os.path.isdir(pathMod):
                 if os.makedirs(pathMod):
-                    print ("I can't create the necessary directory!")
+                    print("I can't create the necessary directory!")
                 else:
-                    saveFile(pathMod+"sito.txt", site, mail, urli)
+                    saveFile(pathMod + "sito.txt", site, mail, urli)
             else:
-                saveFile(pathMod+"sito.txt", site, mail, urli)
+                saveFile(pathMod + "sito.txt", site, mail, urli)
         elif responseCode == 404:
             print("This page doesn't exist!")
         else:
-            print ("Generic error.")
+            print("Generic error.")
     except urllib.request.URLError:
-        print ("There is an error with the link.")
+        print("There is an error with the link.")
 
-def sendMail(site,dir,mail):
+
+def sendMail(site, dir, mail):
     try:
         server = smtplib.SMTP("smtp.gmail.com:587")
         server.starttls()
-        server.login("SiteAlertMailNotification@gmail.com","SiteAlertMailNotificatio")
-        subj = "The site \""+dir+"\" has been changed!"
-        msg = "Subject: "+subj+"\n"+subj+"\nLink: "+site
-        mail=mail.split(";")
+        server.login("SiteAlertMailNotification@gmail.com", "SiteAlertMailNotificatio")
+        subj = "The site \"" + dir + "\" has been changed!"
+        msg = "Subject: " + subj + "\n" + subj + "\nLink: " + site
+        mail = mail.split(";")
         for address in mail:
-            server.sendmail("SiteAlertMailNotification@gmail.com",address,msg)
+            server.sendmail("SiteAlertMailNotification@gmail.com", address, msg)
         server.close()
     except smtplib.SMTPRecipientsRefused:
-        print ("Error with the e-mail destination address.")
+        print("Error with the e-mail destination address.")
+
 
 def checkSite():
-    list=os.listdir(path)
-    if len(list)!=0:
+    list = os.listdir(path)
+    if len(list) != 0:
         for dir in list:
-            pathMod=path+dir+os.sep+"sito.txt"
-            f=open(pathMod,"r");
-            site=f.readline()
-            mail=f.readline()
-            urli=urllib.request.urlopen(stdURL(site))
-            if f.read()==str(urli.read()):
-                print ("The site \"" + dir + "\" hasn't been changed!")
+            pathMod = path + dir + os.sep + "sito.txt"
+            f = open(pathMod, "r");
+            site = f.readline()
+            mail = f.readline()
+            urli = urllib.request.urlopen(stdURL(site))
+            if f.read() == str(urli.read()):
+                print("The site \"" + dir + "\" hasn't been changed!")
             else:
-                print ("The site \"" + dir + "\" has been changed!")
-                addSite(site,dir,mail)
-                sendMail(site,dir,mail)
+                print("The site \"" + dir + "\" has been changed!")
+                addSite(site, dir, mail)
+                sendMail(site, dir, mail)
             f.close()
     else:
-        print ("You haven't checked any site.")
+        print("You haven't checked any site.")
         return True
     return False
 
+
 def numberReq(leng):
-    s=-1
+    s = -1
     displaySites()
     while s < 0 or s > leng:
-        print ("Number of the site: ",)
-        s=int(input())
+        print("Number of the site: ", )
+        s = int(input())
     return s
 
+
 def main():
-    c=1;n=len(sys.argv)
+    c = 1; n = len(sys.argv)
     while True:
-        x="";s="";dirs=os.listdir(path);leng= len(dirs)
-        if c<n:
-            arg=sys.argv[c]
-            if arg=="-a":
-                x=2
-            elif arg=="-b":
-                x=4
-                s="y"
-            elif arg=="-c":
-                x=4
-                s="n"
-            elif arg=="-d":
-                x=5
-            elif arg=="-e":
-                x=6
-            elif arg=="-f":
-                x=3
-            elif arg=="-h":
-                x=-1
+        x = ""; s = ""; dirs = os.listdir(path); leng = len(dirs)
+        if c < n:
+            arg = sys.argv[c]
+            if arg == "-a":
+                x = 2
+            elif arg == "-b":
+                x = 4
+                s = "y"
+            elif arg == "-c":
+                x = 4
+                s = "n"
+            elif arg == "-d":
+                x = 5
+            elif arg == "-e":
+                x = 6
+            elif arg == "-f":
+                x = 3
+            elif arg == "-h":
+                x = -1
                 print("Usage:\n-a -> add a new site\n-b -> continuous check\n-c -> check once\n-d -> delete a site\n-e -> exit\n-h -> print this help\n-s -> show the list of the sites")
-            elif arg=="-s":
-                x=1
-            c+=1
-        if x=="":
+                input("Press enter to continue...")
+            elif arg == "-s":
+                x = 1
+            c += 1
+        if x == "":
             x = choice()
         clearScreen()
-        if x==1:
-            if leng!=0:
+        if x == 1:
+            if leng != 0:
                 displaySites()
             else:
-                print ("You haven't checked any site!")
-        elif x==2:
-            addSite("","","")
-        elif x==3:
+                print("You haven't checked any site!")
+        elif x == 2:
+            addSite("", "", "")
+        elif x == 3:
             if leng != 0:
-                print ("Write the number of the site that you want to fetch.")
-                s=numberReq(leng)-1
-                pathMod=path+dirs[s-1]+os.sep+"sito.txt"
+                print("Write the number of the site that you want to fetch.")
+                s = numberReq(leng) - 1
+                pathMod = path + dirs[s - 1] + os.sep + "sito.txt"
                 if os.path.exists(pathMod):
-                    f=open(pathMod,"r")
-                    addSite(f.readline(),dirs[s],f.readline())
+                    f = open(pathMod, "r")
+                    addSite(f.readline(), dirs[s], f.readline())
                     f.close()
                 else:
-                    print ("No configuration file found.")
+                    print("No configuration file found.")
             else:
-                print ("You haven't checked any site.")
-        elif x==4:
-            if s=="":
-                print ("Do you want to check it continually? (Y/n)")
-                s=input()
+                print("You haven't checked any site.")
+        elif x == 4:
+            if s == "":
+                print("Do you want to check it continually? (Y/n)")
+                s = input()
                 while len(s) == 0 or ( s[0] != 'n' and s[0] != 'y'):
                     if len(s) == 0:
-                        s="y"
+                        s = "y"
                         break
                     else:
-                        print ("Wrong input, do you want to check it continually? (Y/n)")
-                        s=input()
+                        print("Wrong input, do you want to check it continually? (Y/n)")
+                        s = input()
             while True:
-                if checkSite() or s!="y":
-                    s="n"
+                if checkSite() or s != "y":
+                    s = "n"
                     break
                 else:
                     time.sleep(30)
-        elif x==5:
+        elif x == 5:
             if leng != 0:
-                print ("Write the number of the site that you want to delete.")
-                s=numberReq(leng)-1
-                pathMod=path+dirs[s]+os.sep
+                print("Write the number of the site that you want to delete.")
+                s = numberReq(leng) - 1
+                pathMod = path + dirs[s] + os.sep
                 try:
                     shutil.rmtree(pathMod, ignore_errors=True)
-                    print ("Site successfully deleted!")
+                    print("Site successfully deleted!")
                 except shutil.Error:
-                    print ("Something went wrong.")
+                    print("Something went wrong.")
             else:
-                print ("You haven't checked any site!")
-        elif x==6:
+                print("You haven't checked any site!")
+        elif x == 6:
             sys.exit(0)
         input("Press enter to continue...")
+
 
 if __name__ == "__main__":
     main()
